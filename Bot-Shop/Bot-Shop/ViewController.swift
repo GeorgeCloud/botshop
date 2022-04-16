6163508f1bdb201477f092242e1aee36c841e988
GeorgeCloud <net.george.sky@gmail.com>
2022-04-16 13:04:16 -0700

    add: setup up scene delegate

---
+++ Bot-Shop/Bot-Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot-Shop
+//
+//  Created by George Ceja on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
