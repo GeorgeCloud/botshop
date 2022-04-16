4bc67c0db39a8e51dce323f76ed88aebb1d5451e
GeorgeCloud <net.george.sky@gmail.com>
2022-04-16 14:55:45 -0700

    add: setup tab bar view

---
+++ Bot-Shop/Bot-Shop/NewOrderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PastOrderViewController.swift
+//  Bot-Shop
+//
+//  Created by George Ceja on 4/16/22.
+//
+
+import Foundation
+import UIKit
+
+class NewOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
--- Bot-Shop/Bot-Shop/PastOrderViewController.swift
+++ Bot-Shop/Bot-Shop/PastOrderViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  PastOrderViewController.swift
 //  Bot-Shop
 //
 //  Created by George Ceja on 4/16/22.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class PastOrderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .green
     }
-
-
 }
-
